a3e2fbf91f1a545f76358546cb6111455e4e59ad
ZSKYmario <zakiisteen@gmail.com>
2018-10-27 17:55:29 +0800

    lessoon7

--- Phaser/app.js
+++ Phaser/app.js
@@ -1,4 +1,4 @@
- var game = new Phaser.Game(800, 600, Phaser.Auto, '', {preload:, create, update:update})
+ var game = new Phaser.Game(800, 600, Phaser.Auto, '', {preload:preload, create:create, update:update})
 
 var score = 0;
 var lives = 5;
